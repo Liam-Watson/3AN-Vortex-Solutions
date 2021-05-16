c6f3595cf16e892e33ef261cb79781ea6e05cdcf
Liam Watson <liam@watson.org.za>
2021-05-16 17:34:09 +0200

    More description in the tex doc around data, refactored and commented code and added it to tex

--- proj1_NK_FD.m
+++ proj1_NK_FD.m
@@ -1,88 +1,75 @@
-
 hold off
 clf
 clc
 clear
-hold on
 
-%h = (b-a)/N;
-
-%r = (a:h:b)';
-
-%Initial guesses
-%u = r/b;
-
-
-%n = 1; %vorticity 
-%file = fopen("inf.txt", 'w');
-for n = [1,2,3] %Here we loop over our hyperparameters and test for
-%stability i.e. N, b, a,1000 etc.
-    a = 0; %This is our starting position
-    b = 7; %infity
-    N = 50;
-    if n == 1
-        a = 0.1;
-    end
-    h = (b-a)/N;
-    h2 = h*h;
-    r = (a:h:b)';
-    %u = r/b;
-    %u = 1./(1+exp(-r));
-    u = log(r + 1)/log(b+1) -a; 
+for n = [1,2,3] % Loop over all n=1,2,3 and obtain numerical solutions
+    a = 0; %The left BC
+    b = 4; %infity (the right BC)
+    N = 50; %Define number of mesh points
+    h = (b-a)/N; %Define the mesh step size according to scheme
+    h2 = h*h; %Simplification of code
+    r = (a:h:b)'; %Set up r mesh with step size h between a and b
+    u = r/b; %Initial guess is linear, other guesses are bellow
+    %u = log(r + 1)/log(b+1) -a; 
     %u = -exp(-(r)) + 1 ;
-    
     %u = 1./(1+exp(-r + b/2)); 
-    rhs=[u(1);u(1:N-1)-2*u(2:N)+u(3:N+1) + h2.*f(r,u,h, N, n);u(N+1)-1];
-    tol = 1;
-    count = 1;
-    %n = 2
-    while count < 100000 && tol > 1e-7
+    rhs=[u(1);u(1:N-1)-2*u(2:N)+u(3:N+1) + h2.*f(r,u,h, N, n);u(N+1)-1]; %Set initial RHS based on guess
+    tol = 1; %Initialize tolerance value
+    count = 1; %Initialize iteration counter
+    while count < 100 && tol > 1e-7
         dd = [0; -2 - h2.*q(r,u,h,N, n) ; 0];
         upper = diag([0;1 - (h*p(r,u,h,N))./2],1);
         lower = diag([1 + (h*p(r,u,h,N))./2;0],-1);
         J = diag(dd,0) + upper + lower;  %Set three diagonal
-        J(1,1)=1; %J(1,2)=0; J(1,3)=0; %Set BC 1
-        J(N+1,N+1)=1; %J(N+1,N)=0; J(N+1,N-1)=0; %Set BC 2
+        J(1,1)=1; %Set BC 1
+        J(N+1,N+1)=1; %Set BC 2
         z = -J\rhs; %Solve for correction
-        u = u + z; 
-        rhs=[u(1);(u(1:N-1)-2*u(2:N)+u(3:N+1)) + h2.*f(r,u,h,N,n);u(N+1)-1]; 
+        u = u + z; %Update u with new correction
+        rhs=[u(1);(u(1:N-1)-2*u(2:N)+u(3:N+1)) + h2.*f(r,u,h,N,n);u(N+1)-1]; %Update RHS values
         tol = norm(rhs); %Display tolerance
-        %plot(r,u)
-        %pause(0.05);
-        count = count+1; 
+        fig1 = figure(1);
+        plot(r,u); %Plot updated curve of u
+        hold off;
+        pause(0.1);
+        count = count+1; %Increment iteration counter
     end
-%    bool = tol < 1e-5;%tolerence to be considered stable
-%    fprintf(file,'%6.2f %12.8f\n', b, bool); %Write stability data
-    disp(tol)
-    plot(r,u)
-    legend("n=1", "n=2", "n=3")
-    title("NK for b = 7, tol = 1e-7, logarithm guess, N = 50")
-    xlabel('r')
-    ylabel('u')
-end %End of for loop
-%fclose(file);500
+    %Plot final solution u for each n
+    fig2 = figure(2);
+    plot(r,u);
+    hold on;
+    legend("n=1", "n=2", "n=3");
+    title("NK for b = 7, tol = 1e-7, logarithm guess, N = 50");
+    xlabel('r');
+    ylabel('u');
+    pause(1);
+end %End of n for loop
+
 %This is f in y'' = f
 function [fu] = f(r, u, h, N, n)
-    uBefore = u(1:N-1);
-    uAfter = u(3:N+1);
-    u = u(2:N);
-    fu = 1*((1./r(2:N)).*((uAfter - uBefore)/(2*h)) + ((u)./(1-u.^2)).*(((uAfter - uBefore)/(2*h)).^2 - n^2./(r(2:N).^2)) + u.*(1-u.^2));
+    uBefore = u(1:N-1); %This is u_{j-1}
+    uAfter = u(3:N+1); %This is u_{j+1}
+    u = u(2:N); %This is u_{j}
+    %Evaluate and return f(2:N). Note the FD approximations of u' 
+    fu = (1./r(2:N)).*((uAfter - uBefore)/(2*h)) + ((u)./(1-u.^2)).*(((uAfter - uBefore)/(2*h)).^2 - n^2./(r(2:N).^2)) + u.*(1-u.^2);
 end 
-%This is p in a linear ODE
+
+%This is p(r) in a linear kantorovich equations
 function [p] = p(r,u,h, N)
-    uBefore = u(1:N-1);
-    uAfter = u(3:N+1);
-    uu = u(2:N);
-    r = r(2:N);
+    uBefore = u(1:N-1); %This is u_{j-1}
+    uAfter = u(3:N+1); %This is u_{j+1}
+    uu = u(2:N); %This is u_{j}
+    r = r(2:N); %Find r excluding the end points
+    %Evaluate and return p(2:N)
     p = -1*(1./r + (2*uu.*((uAfter-uBefore)/(2*h)))./(1-uu.^2));
 end
 
-
-%q(r)
+%This is q(r) in the linear kantorovich equations
 function [q] =  q(r,u, h , N, n)
-    r = r(2:N);
-    uBefore = u(1:N-1);
-    uAfter = u(3:N+1);
-    uu = u(2:N);
+    r = r(2:N); %Find r excluding the end points
+    uBefore = u(1:N-1);  %This is u_{j-1}
+    uAfter = u(3:N+1); %This is u_{j+1}
+    uu = u(2:N); %This is u_{j}
+    %Evaluate and return q(2:N)
     q= -1*(-3*uu.^2 + 1 + 1./((1-uu.^2)).*(((uAfter-uBefore)/(2*h)).^2 - (n^2)./(r.^2) )); 
 end
--- proj1_Shoot.m
+++ proj1_Shoot.m
@@ -2,74 +2,67 @@ hold off
 clf
 clc
 clear
-hold on
-%fig = figure("Name", "Nonlinear shooting for a = 0.1, b = 15, tol = 1e-7")
-%figure(fig)
-a = 0.1; % Boundary one
-b = 15; % infinity
-n = 2;
-%p = 0.2; %Arbitray initial shooting parameter
 
-%file = fopen("inf.txt", 'w');
-%fprintf(file,'%6.2f %12.8f\n', n);
 for n = [1,2,3]
-    ph = 4; %bisection
-    pl = -4;
-    p = (ph + pl)/2; %bisection
-    tol = 1;
-    i = 1;
-    u0 = [0,p,0,1];
-while i < 100 && tol > 1e-7
-   [r,u] = ode45(@(r,u) shoot(r,u,n), [a:0.2:b], u0,n);
-   %disp(u(end,3))
-   %plot(r,u(:,1));
-   %pause(0.1)
-   %disp((u(end,4)))
-   %tol = abs(1-u(end,1));
-   tor = u(end,1);
-   if(tor > 1)
-      ph = p;
-      p = (pl + ph)/2;
-      u0 = [0,p,0,1];   
-   end
-   if(tor < 1)
-      pl = p;
-      p = (pl + ph)/2;
-      u0 = [0,p,0,1];  
-   end
-   %disp(p)
-   %p = p - ((u(end,1)-1)/(u(end,3)));
-   %u0 = [0,p,0,1];
-   %disp([p,ph,pl])
-   %disp(u0);
-   i = i + 1;
-   %plot(r,u(:,1))
-   %pause(0.1);
+    a = 0.1; % Boundary one
+    b = 15; % infinity [1,2,3]
+    ph = 4; %bisection upper bound
+    pl = -4; %Bisection lower bound
+    p = (ph + pl)/2; %bisection first mid point
+    %p = 0.2; %Arbitray initial shooting parameter used for NM
+    tol = 1; %Set initial tolerance
+    i = 1; %Counter for number of iterations
+    u0 = [0,p,0,1]; %Set initial conditions for our system
+    step_size = 0.2; %Set step size 
+    while i < 100 && tol > 1e-7
+        %Integrate the system using runge-kutta method
+        [r,u] = ode45(@(r,u) shoot(r,u,n), [a:step_size:b], u0,n);
+        tol = abs(1-u(end,1)) %Display the tolerance
+        tor = u(end,1); %Purely aesthetic
+        %Bisection scheme for updating p.
+        if(tor > 1)
+            ph = p; %Move upper bound
+            p = (pl + ph)/2; %Bisect
+            u0 = [0,p,0,1]; %Update IC's with new p value
+        end
+        if(tor < 1) %Note that we exclude tor = 1 explicitly as if tor = 1 we are finished.
+            pl = p; %Move lower bound
+            p = (pl + ph)/2; %Bisect
+            u0 = [0,p,0,1]; %Update IC's with new p value
+        end
+        %End of Bisection scheme
+        
+        %NM scheme. Note that we use bisection primarily for stability
+        %p = p - ((u(end,1)-1)/(u(end,3))); %Update p according to NM scheme
+        %u0 = [0,p,0,1]; %Update the IC's with new p value
+        
+        i = i + 1; %Update iteration counter
+        figure(1);plot(r,u(:,1)); %Plot the updated solution curve
+        hold off;
+        pause(0.1);
+    end
+    fig2 = figure(2);
+    plot(r,u(:,1))
+    hold on;
+    legend("n=1", "n=2", "n=3");
+    title("Nonlinear shooting for a = 0.1, b = 15, tol = 1e-7");
+    xlabel('r');
+    ylabel('u');
+    pause(1);
 end
-%    bool = tol < 1e-5;%tolerence to be considered stable
-%    fprintf(file,'%6.2f %12.8f\n', b, bool);
 
-plot(r,u(:,1))
-legend("n=1", "n=2", "n=3")
-title("Nonlinear shooting for a = 0.1, b = 15, tol = 1e-7")
-xlabel('r')
-ylabel('u')
-end
-
-%fclose(file);
+%The system of equations for u and z. Note z is only needed for NM scheme
 function [du] = shoot(r,u, n)
-    %n = 2;
     uu = u(1);
     v = u(2);
-    z = u(3);
+    z = u(3);%Extract the IC's
     w = u(4);
-    
-    
-    du(1,1) = v; %
+    %Update scheme needed for integrating u
+    du(1,1) = v;
     du(2,1) = (-1/r)*v  - (uu/(1-uu^2))*(v^2-(n^2)/(r^2)) - uu*(1-uu^2);
-    du(3,1) = w; % 
-    du(4,1) = z*( -((1 + uu^2)/((1-uu^2)^2))*(v^2 - (n^2)/(r^2)) - 1 + 3*uu^2) + w*(-1/r  - ((2*uu)*v)/(1-uu^2)) ;
-    
+    %Update scheme needed for integrating z
+    du(3,1) = w;
+    du(4,1) = z*( -((1 + uu^2)/((1-uu^2)^2))*(v^2 - (n^2)/(r^2)) - 1 + 3*uu^2) + w*(-1/r  - ((2*uu)*v)/(1-uu^2));
 end
 
 
